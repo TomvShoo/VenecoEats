5f8ea0a8da94a664de88d5c4a36fabc274b743f6
Nicolas Molina Monroy <hi@nicobytes.com>
2021-11-28 13:34:26 -0400

    :construction: work

---
+++ src/app/map/map.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MapPageRoutingModule } from './map-routing.module';
+
+import { MapPage } from './map.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    MapPageRoutingModule,
+    GoogleMapsModule
+  ],
+  declarations: [MapPage]
+})
+export class MapPageModule {}
---
+++ src/app/map/map.page.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Geolocation } from '@capacitor/geolocation';
+import { GoogleMap } from '@angular/google-maps';
+
+interface MyPoint {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  title: string;
+  image: string;
+  text: string;
+}
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+})
+export class MapPage implements OnInit {
+
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(IonSlides, { static: false }) slides: IonSlides;
+
+  center: google.maps.LatLngLiteral;
+  points: MyPoint[] = [
+    {
+      position: {
+        lat: -17.386378,
+        lng: -66.1628018,
+      },
+      title: 'Parque De la Familia',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipOCgzq_0DYB9AxD-ItTG01x2csLsSfWsawBCypc=w408-h306-k-no',
+      text: 'Animi voluptatem, aliquid impedit ratione placeat necessitatibus quisquam molestiae obcaecati laudantium?',
+    },
+    {
+      position: {
+        lat: -17.4005556,
+        lng: -66.1741667,
+      },
+
+      title: 'Mariscal Santa Cruz',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipMGZeu88O8uZvFOX9PKug7gz-VRhhiXQ78hAFZU=w408-h306-k-no',
+      text: 'Animi voluptatem, aliquid impedit ratione placeat necessitatibus quisquam molestiae obcaecati laudantium?',
+    },
+    {
+      position: {
+        lat: -17.3810618,
+        lng: -66.1550974,
+      },
+      title: 'Parque de Educación Vial',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipPIXxrXfshAD6eHbkGScPdNqYBwfJ6ol4qriq2n=w408-h306-k-no',
+      text: 'Animi voluptatem, aliquid impedit ratione placeat necessitatibus quisquam molestiae obcaecati laudantium?',
+    },
+    {
+      position: {
+        lat: -17.4128145,
+        lng: -66.158299,
+      },
+      title: 'Parque Kanata',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipOJOq3vm1Gfpa3d4dPR_ca2C240J_PBv701zRAE=w408-h544-k-no',
+      text: 'Animi voluptatem, aliquid impedit ratione placeat necessitatibus quisquam molestiae obcaecati laudantium?',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.getPosition();
+  }
+
+  async getPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.center = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude,
+    };
+  }
+
+  async onSlideDidChange() {
+    const currentSlide = await this.slides.getActiveIndex();
+    const point = this.points[currentSlide];
+    this.map.panTo(point.position);
+  }
+}
