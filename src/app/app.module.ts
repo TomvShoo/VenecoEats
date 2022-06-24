a2e1d6cf692e1d1b9e0b5487c3a09728c9887a59
juan diaz <juai.diaz@duocuc.cl>
2022-06-24 10:05:01 -0400

    UB-3 login actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,  
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    //aqui van los servicios
+    ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
