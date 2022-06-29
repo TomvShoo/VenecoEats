1e1a241726ea341f93bdb5457d7723d04fb461b4
juan diaz <juai.diaz@duocuc.cl>
2022-06-28 21:56:56 -0400

    cambios menores

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AlertController } from "@ionic/angular";
 
 @Component({
